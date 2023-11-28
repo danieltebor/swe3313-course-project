1dc2978aba3bba1c506abcf892b48c1b03b3eaa8
2GrayZ2 <145519917+2GrayZ2@users.noreply.github.com>
2023-11-28 17:26:45 -0500

    Update PasswordMismatchException

--- HanksMineralEmporium/Service/AuthenticationService/Exception/PasswordMismatchException.cs
+++ HanksMineralEmporium/Service/AuthenticationService/Exception/PasswordMismatchException.cs
@@ -1,13 +1,14 @@
 namespace HanksMineralEmporium.Service.AuthenticationService.Exception;
+
 /// <summary>
 /// Represents an exception thrown when two passwords do not match during authentication.
 /// </summary>
-    class PasswordMismatchException : System.Exception
-    {
+class PasswordMismatchException : System.Exception
+{
      /// <summary>
     /// Initializes a new instance of the <see cref="PasswordMismatchException"/> class with a specified error message.
     /// </summary>
     /// <param name="message">The error message that explains the reason for the exception.</param>
     public PasswordMismatchException(string message) : base(message){}
 
-    }
+}
