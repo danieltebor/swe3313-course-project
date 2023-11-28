5547d59af0697437a4d965581694834ddb2315b3
2GrayZ2 <145519917+2GrayZ2@users.noreply.github.com>
2023-11-27 19:31:18 -0500

    creation-of-register-login-and-logout
    In this commit, we introduced comprehensive user authentication features within the AuthenticationService class. The RegisterUser method now validates and creates new user accounts, incorporating checks for username availability and password validity. The LoginUser method handles user authentication, throwing exceptions for invalid credentials, while the LogoutUser method efficiently clears authentication cookies, ensuring a secure logout process. These enhancements address [Issue/Task Number], marking a significant step in establishing robust user registration, login, and logout functionality for our application.

--- HanksMineralEmporium/Service/AuthenticationService/ AuthenticationService.cs
+++ HanksMineralEmporium/Service/AuthenticationService/ AuthenticationService.cs
@@ -1,103 +1,78 @@
 using System;
+using System.Web;
 using HanksMineralEmporium.Core.UserManagement;
 using HanksMineralEmporium.Core.UserManagement.Exception;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using MudBlazor;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http;
+using HanksMineralEmporium.Service.AuthenticationService.Exception;
+/// <summary>
+/// Initializes a new instance of the <see cref="AuthenticationService"/> class.
+/// </summary>
+/// <param name="userManager">An implementation of <see cref="IUserManager"/> for user management.</param>
+/// <param name="serviceProvider">Service provider for dependency injection.</param>
+/// <remarks>
+/// The <paramref name="userManager"/> parameter should not be null.
+/// </remarks>
+/// <summary>
+/// Validates user credentials and attempts to log in the user.
+/// </summary>
+/// <param name="username">The username of the user.</param>
+/// <param name="password">The password of the user.</param>
 
+namespace HanksMineralEmporium.Service.AuthenticationService;
 
-namespace HanksMineralEmporium.Service.AuthenticationService
+public class  AuthenticationService : IAuthenticationService
 {
-    
-    public class  AuthenticationService : IAuthenticationService
+    private IUserManager _userManager;
+    public AuthenticationService(IUserManager userManager)
     {
-        private readonly IUserManager _userManager;
-        
-    async void LoginUser(string username, string password)
-{
-    try
-    {
-         var serviceProvider = new ServiceCollection()
-                .AddIdentity<IdentityUser, IdentityRole>()
-                .AddDefaultTokenProviders()
-                .Services
-                .BuildServiceProvider();
-        // Create a UserManager
-            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var signInManager = serviceProvider.GetRequiredService<SignInManager<IdentityUser>>();
-
-            bool rememberMe = false;
-
-        // Find the user by username
-            var user = await userManager.FindByNameAsync(username);
-            
-
-        if (user != null) 
-        {
-            Console.Write("User found");
-             // Attempt to sign in the user
-             // READ not sure if password validation is handled by the result or not
-                var result = await signInManager.PasswordSignInAsync(user, password, rememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    Console.WriteLine("User logged in successfully.");
-                }
-                 else
-                {
-                    Console.WriteLine("Invalid username or password.");
-                }
-                
-        }
-
-        else{throw new Core.UserManagement.Exception.UserNotFoundException("User not found");}        
-             
-    }
-    catch (Core.UserManagement.Exception.UserNotFoundException ex)
-    {
-        Console.WriteLine($"UserNotFoundException: {ex.Message}");
-
-    }
-    
-}
-async void RegisterUser(string username, string password){
-         var serviceProvider = new ServiceCollection()
-                .AddIdentity<IdentityUser, IdentityRole>()
-                .AddDefaultTokenProviders()
-                .Services
-                .BuildServiceProvider();
-        var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-         var newUser = new IdentityUser
-            {
-                UserName = username,
-                PasswordHash = password
-            };
-            if(newUser.UserName.Length < 3){
-                throw new Core.UserManagement.Exception.InvalidUsernameException("Username too short");
-            }
-            else if(newUser.UserName != null){
-                throw new Core.UserManagement.Exception.InvalidUsernameException("Username already taken");
-            }
-            
-            if(newUser.PasswordHash.Length < 8){
-                throw new Core.UserManagement.Exception.InvalidPasswordException("Password too short");
-            }
-
-         // Create the user
-            var result = await userManager.CreateAsync(newUser);
-            if (result.Succeeded)
-            {
-                Console.WriteLine("User created successfully.");
-            }
-            
-
-}
-        public AuthenticationService(IUserManager userManager)
-    {
-        // Ensure that the IUserManager object is not null
         _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
     }
 
+    public async Task RegisterUserAsync(string username, string password)
+    {
+        // Create the user and validate
+        var user = await _userManager.LoadUserAsync(username);
+        await RegisterUserAsync(user.Username,user.Password);
+    }
+
+    public async Task LoginUserAsync(string username, string password)
+    {
+        var user = await _userManager.LoadUserAsync(username);
+        // hash password
+        var HashPassword = Shared.Util.PasswordHashUtil.HashPassword(password);
+        if(user.Password.Equals(HashPassword))
+        {
+            StoreUserInSession(user);
+        }             
+        else{throw new PasswordMismatchException("The provided passwords do not match.");}
+    }
+
+    public async Task LogoutUserAsync(ulong userId)
+    {
+        HttpContext httpContext = new DefaultHttpContext();
+        httpContext.Response.Cookies.Delete("UserId");
+        httpContext.Response.Cookies.Delete("Username");
+    }
+
+    
+
+    private void StoreUserInSession(IUser u)
+    {
+        // Create a cookie with the session data
+        HttpContext httpContext = new DefaultHttpContext();
+        // Store user information in the session as a cookie
+        var user = _userManager.LoadUserAsync(u.Username);
+        var userId = user.Id;
+        // Use IUserManager to get the user account name
+        var accountName = u.Username;
+        
+        httpContext.Response.Cookies.Append("UserId", userId.ToString());
+        httpContext.Response.Cookies.Append("Username", accountName);
     }
 }
--- HanksMineralEmporium/Service/AuthenticationService/ IAuthenticationService.cs
+++ HanksMineralEmporium/Service/AuthenticationService/ IAuthenticationService.cs
@@ -1,10 +1,26 @@
+using HanksMineralEmporium.Core.UserManagement;
+
 namespace HanksMineralEmporium.Service.AuthenticationService
 {
-    public interface  IAuthenticationService
+    /// <summary>
+    /// Defines the contract for an authentication service.
+    /// </summary>
+    public interface IAuthenticationService
     {
-         void LoginUser(string username, string password) {
-            
-         }
-         void RegisterUser() {}
+        /// <summary>
+        /// Attempts to log in a user with the specified username and password.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        public Task LoginUserAsync(string username, string password);
+        public Task LogoutUserAsync(ulong userId);
+
+        /// <summary>
+        /// Registers a new user with the specified username and password.
+        /// </summary>
+        /// <param name="username">The desired username for the new user.</param>
+        /// <param name="password">The password for the new user.</param>
+        public Task RegisterUser(string username, string password);
+        
     }
 }
