7ba2bbda79b2580b4b16b7845d3b55b018fc4c44
Daniel Tebor <danieltebor@gmail.com>
2023-11-29 01:33:28 -0500

    Fixed missing comment

--- HanksMineralEmporium/Service/InventoryService/InventoryService.cs
+++ HanksMineralEmporium/Service/InventoryService/InventoryService.cs
@@ -1,7 +1,7 @@
 namespace HanksMineralEmporium.Service.InventoryService;
 
 /// <summary>
-/// 
+/// Service that provides <see cref="IItem"/> objects.
 /// </summary>
 public class InventoryService : IInventoryService
 {
